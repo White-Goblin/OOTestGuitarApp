a7087baa4ddccc24f1df2d291100182d61637e4b
White-Goblin <ap4thy1@gmail.com>
2016-10-01 19:30:55 -0700

    updated inventory class

--- Guitar Inventory/src/Inventory.java
+++ Guitar Inventory/src/Inventory.java
@@ -27,6 +27,27 @@ public class Inventory {
 		return null;
 	}
 	
-	
-	
+	public Guitar search(Guitar searchGuitar) {
+		for (Iterator i = guitars.iterator(); i.hasNext(); ) {
+			Guitar guitar = (Guitar) i.next();
+			//Ignore serial number since it's unique
+			//Ignore price since that's unique
+			String builder = searchGuitar.getBuilder();
+			if ((builder != null) && (!builder.equals("")) && (!builder.equals(guitar.getBuilder())))
+				continue;
+			String model = searchGuitar.getModel();
+			if ((model != null) && (!model.equals("")) && (!model.equals(guitar.getModel())))
+				continue;
+			String type = searchGuitar.getType();
+			if ((type != null) && (!type.equals("")) && (!type.equals(guitar.getType())))
+				continue;
+			String backWood = searchGuitar.getBackWood();
+			if ((backWood != null) && (!backWood.equals("")) && (!backWood.equals(guitar.getBackWood())))
+				continue;
+			String topWood = searchGuitar.getTopWood();
+			if ((topWood != null) && (!topWood.equals("")) && (!topWood.equals(guitar.getTopWood())))
+				continue;
+		}
+		return null;
+	}
 }
